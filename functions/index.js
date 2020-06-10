a6e168df80012332154c9121ae8a0209d89d7f33
bill <manbil03@gmail.com>
2020-06-10 13:35:44 +0930

    api:added create ascent and read ascent by id

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+var serviceAccount = require('./permissions.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://kitt-da2fa.firebaseio.com"
+});
+
 const express = require('express')
-const cores = require('cors')
 const app = express()
+const db = admin.firestore()
+
+const cors = require('cors')
+app.use(cors({ origin:true }))
 
 
 //Routes
@@ -11,9 +21,43 @@ app.get('/hello', (req, res) => {
 })
 
 //Create
+app.post('/api/create', (req, res) => {
+    (async () => {
 
+        try {
+            await db.collection('apiAscents').doc('/' + req.body.id + '/')
+            .create({
+                name: req.body.name,
+                grade: req.body.grade,
+                ascentType: req.body.ascentType
+            })
+
+            return res.status(200).send()
+        } catch (error) {
+            console.log(error) 
+            return res.status(500).send(error) 
+        }
+
+    })()
+})
 
 //Read
+app.get('/api/read/:id', (req, res) => {
+    (async () => {
+
+        try {
+            const document = db.collection('apiAscents').doc(req.params.id)
+            let ascent = await document.get()
+            let response = ascent.data()
+
+            return res.status(200).send(response)
+        } catch (error) {
+            console.log(error) 
+            return res.status(500).send(error) 
+        }
+
+    })()
+})
 
 
 //Update
